621261fa035a2073f0b59a97ffc8df276084a67b
Mike <52970344+Developer-Mike@users.noreply.github.com>
2022-01-09 13:40:55 +0100

    Finished navigation logic

--- src/main/java/mike/waypoints/mixin/WaypointNavigator.java
+++ src/main/java/mike/waypoints/mixin/WaypointNavigator.java
@@ -3,6 +3,7 @@ package mike.waypoints.mixin;
 import com.mojang.bridge.launcher.SessionEventListener;
 import mike.waypoints.client.Waypoint;
 import mike.waypoints.client.WaypointsClient;
+import net.fabricmc.loader.impl.lib.sat4j.core.Vec;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.util.math.Vector2f;
 import net.minecraft.client.util.math.Vector3d;
@@ -37,21 +38,22 @@ public abstract class WaypointNavigator  implements SessionEventListener {
                 player.getBlockPos().getZ() - waypoint.pos.getZ()
         );
 
-        double playerAngle = player.getRotationClient().y;
+        double playerAngle = player.getRotationClient().y + 180;
         double angle = angleToWaypoint + playerAngle;
         boolean isPositive = angle >= 0;
-        while (angle > 360 || angle < -360) {
+        while (angle > 180 || angle < -180) {
             angle -= isPositive ? 360 : -360;
         }
         angle += 45;
         if (angle < 0 || angle > 90) {
-            LiteralText text = new LiteralText(navigationStringPlaceholder.repeat(navigationStringLength));
+            LiteralText text = new LiteralText(navigationStringPlaceholder.repeat(navigationStringLength + 3));
             return text.formatted(Formatting.RED);
         }
 
-        int navigationIndicatorPosition = (int) ((angle / 90) * navigationStringLength);
+        int navigationIndicatorPosition = navigationStringLength - (int) ((angle / 90) * navigationStringLength);
 
-        int distance = player.getBlockPos().getManhattanDistance(waypoint.pos);
+        Vec3d posOffset = new Vec3d(waypoint.pos.getX() - player.getBlockPos().getX(), 0, waypoint.pos.getZ() - player.getBlockPos().getZ());
+        int distance = (int) Math.sqrt(posOffset.x * posOffset.x + posOffset.z * posOffset.z);
 
         LiteralText text = new LiteralText(navigationStringPlaceholder.repeat(navigationIndicatorPosition) + " " + distance + "m " + navigationStringPlaceholder.repeat(navigationStringLength - navigationIndicatorPosition));
         return text.formatted(Formatting.RED);
